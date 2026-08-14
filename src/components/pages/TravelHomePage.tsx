fc993a2ac9cae166bfd41eac98ccd2b50ec562ff
Wix Remote Machine <support@wix.com>
2026-08-14 09:01:06 +0000

    Manual code changes

--- src/components/pages/TravelHomePage.tsx
+++ src/components/pages/TravelHomePage.tsx
@@ -1149,7 +1149,7 @@ const HOME_STYLES = String.raw`
   flex-direction: column;
   justify-content: center;
   padding: clamp(15px, 1.45vw, 25px);
-  padding-left: calc(clamp(15px, 1.45vw, 25px) + 12px);
+  padding-left: calc(clamp(15px, 1.45vw, 25px) + 12px) !important;
   background: var(--tet-navy-deep);
 }
 
@@ -3127,7 +3127,7 @@ const HOME_STYLES = String.raw`
   min-height: 235px;
   margin: 0;
   padding: clamp(15px, 1.45vw, 25px);
-  padding-left: calc(clamp(15px, 1.45vw, 25px) + 12px);
+  padding-left: calc(clamp(15px, 1.45vw, 25px) + 12px) !important;
   display: flex;
   flex-direction: column;
   justify-content: center;
