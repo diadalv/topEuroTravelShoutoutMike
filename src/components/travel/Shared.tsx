84e6ace49a69376effbcb548aae4e18da657b167
Wix Remote Machine <support@wix.com>
2026-09-02 12:38:27 +0000

    Manual code changes

--- src/components/travel/Shared.tsx
+++ src/components/travel/Shared.tsx
@@ -33,9 +33,8 @@ const navigation: NavigationItem[] = [
   },
   { label: 'SERVICES', to: '/services' },
   { label: 'MICE & GROUPS', to: '/mice-groups' },
+  { label: 'EXPERIENCES', to: '/experiences' },
   { label: 'EXCURSIONS', to: '/excursions' },
-  { label: 'BLOG', to: '/blog' },
-  { label: 'FAQ', to: '/faq' },
 ];
 
 export function SiteHeader() {
