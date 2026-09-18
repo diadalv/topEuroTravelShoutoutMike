9c2fb19ace941ed61f0b0e5b60d0b3802c473f8e
Wix Remote Machine <support@wix.com>
2026-09-18 13:36:35 +0000

    Manual code changes

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,10 +1,16 @@
 export const Head = () => {
-  return (
-    <>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {/* Fonts */}
-      <link rel="preconnect" href="https://static.parastorage.com" />
-    </>
-  );
-};
+    return (
+          <>
+                <meta charSet="UTF-8" />
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                            {/* Performance hints for the primary Wix media origins. */}
+                                  <link rel="preconnect" href="https://static.parastorage.com" crossOrigin="anonymous" />
+                                        <link rel="preconnect" href="https://static.wixstatic.com" crossOrigin="anonymous" />
+                                              <link rel="preconnect" href="https://video.wixstatic.com" crossOrigin="anonymous" />
+                                                  </>
+    );
+  };
+
+    )
+}
